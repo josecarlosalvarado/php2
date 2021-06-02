4072cacfe7dfa4c95bb24500e01c7184e1e5bf4a
josecarlosalvarado <72572799+josecarlosalvarado@users.noreply.github.com>
2021-06-02 12:56:30 +0200

    Add files via upload

--- ejercicios.php
+++ ejercicios.php
@@ -1,1 +1,25 @@
 <?php
+
+
+declare (strict_types=1);
+
+function sumArray(): int {
+    $input = [3, 6, 1, -5, 0, 4];
+
+    return array_sum($input);
+}
+
+function findMax(array $array): int {
+    return max($array);
+} 
+
+// return array_sum(array_column($people, 'age')/count($array));
+function averageAge(array $array): float {
+    $sum = 0;
+    foreach($array as $person) {
+        $sum+=$person['age'];
+    };
+    $totalAge = $sum / count($array);
+    return $totalAge;
+}
+
